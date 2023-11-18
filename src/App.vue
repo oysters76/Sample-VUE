ffd29f5dfa4d0421ad7feb7e5f26ebbb0ec03e3e
Chirath <chirath.nishshanka@gmail.com>
2023-11-18 10:12:10 +0530

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const listvalue = ref("");
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+items.value.push({message:"test"})
+
+function enterValue(event:Event){
+ items.value.push({message:listvalue.value})
+ listvalue.value = "";
+}
+
+</script>
+
+<template>
+  <input type="text" id="textbox" v-model="listvalue">
+  <label>The value is : {{ listvalue }}</label>
+  <button @click="enterValue">Enter</button>
+  <ul>
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
